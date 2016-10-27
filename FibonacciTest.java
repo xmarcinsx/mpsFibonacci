1f8595c5cb986d2bd295f141eca8f6634b9247c4
xmarcinsx <xmarcinsx@gmail.com>
2016-10-27 14:05:32 +0200

    Add files via upload

---
+++ FibonacciTest.java
@@ -0,0 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Created by User on 2016-10-27.
+ */
+public class FibonacciTest {
+    public static void main(String[] args){
+
+        int number = 1;
+        FibonacciPower fp = new FibonacciPower();
+
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Type an integer number: ");
+            try {
+                number = sc.nextInt();
+            } catch (InputMismatchException ex) {
+                System.out.println("Wrong number!");
+                return;
+            }
+            System.out.println(fp.fib(number));
+        } while (true);
+    }
+}
